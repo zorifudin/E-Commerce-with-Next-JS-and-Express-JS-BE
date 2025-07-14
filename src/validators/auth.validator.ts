499607abdc6e37702c74447461ffa6a506fdeb09
zorifudin <zorifimaduddin@gmail.com>
2025-07-14 15:22:22 +0700

    feat:add register

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+export const validateRegister = [
+  body("firstName")
+    .notEmpty()
+    .withMessage("First name is required")
+    .isString()
+    .withMessage("First name must be a string")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("First name must be between 2 and 50 characters"),
+
+  body("lastName")
+    .notEmpty()
+    .withMessage("Last name is required")
+    .isString()
+    .withMessage("Last name must be a string")
+    .isLength({ min: 2, max: 50 })
+    .withMessage("Last name must be between 2 and 50 characters"),
+
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please provide a valid email address")
+    .normalizeEmail(),
+
+  body("phoneNumber")
+    .notEmpty()
+    .withMessage("Phone number is required")
+    .isMobilePhone("any")
+    .withMessage("Please provide a valid phone number"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
+    .withMessage(
+      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+    ),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send({ message: errors.array()[0].msg });
+      return;
+    }
+    next();
+  },
+];
