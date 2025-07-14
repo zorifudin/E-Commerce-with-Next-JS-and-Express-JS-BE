2d2f02ce51d0e1230ccdfa5a9bd194401479a8be
Abdurrohman Zorif <zorifimaduddin@gmail.com>
2025-07-14 19:30:31 +0700

    feat:add login (#3)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { prisma } from "../lib/prisma";
-import { RegisterService } from "../services/auth/get-register.service";
+import { RegisterService } from "../services/auth/register.service";
+import { LoginService } from "../services/auth/login.service";
 
 export const registerController = async (
   req: Request,
@@ -9,6 +10,19 @@ export const registerController = async (
 ) => {
   try {
     const result = await RegisterService(req.body);
+    res.status(200).send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const loginController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const result = await LoginService(req.body);
     res.status(200).send(result);
   } catch (error) {
     next(error);
--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -50,3 +50,31 @@ export const validateRegister = [
     next();
   },
 ];
+
+export const validateLogin = [
+  body("email")
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Please provide a valid email address")
+    .normalizeEmail(),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
+    .withMessage(
+      "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+    ),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).send({ message: errors.array()[0].msg });
+      return;
+    }
+    next();
+  },
+];
